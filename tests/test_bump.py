9afb55155a9a72c39559f086477f6beb0914de07
Lucas L. S. Haine <lucaslshaine@gmail.com>
2023-04-03 23:41:43 -0300

    test: create class for development releases

--- tests/test_bump.py
+++ tests/test_bump.py
@@ -359,3 +359,34 @@ class TestIncreaseMicro:
         version = toml.load(self.tmp_file.name)["project"]["version"]
 
         assert version == "1.4.10rc"
+
+
+class TestMarkAsDev:
+    tmp_file = tmp.NamedTemporaryFile()
+
+    def test_add_dev(self):
+        with open(self.tmp_file.name, "w") as file:
+            file.write('[project]\nversion = "1.5.2"\n')
+
+        result = sp.run([BUMP, self.tmp_file.name, "-d"])
+        version = toml.load(self.tmp_file.name)["project"]["version"]
+
+        assert version == "1.5.2.dev"
+
+    def test_increase_dev(self):
+        with open(self.tmp_file.name, "w") as file:
+            file.write('[project]\nversion = "1.5.2.dev"\n')
+
+        result = sp.run([BUMP, self.tmp_file.name, "-d"])
+        version = toml.load(self.tmp_file.name)["project"]["version"]
+
+        assert version == "1.5.2.dev1"
+
+    def test_remove_dev(self):
+        with open(self.tmp_file.name, "w") as file:
+            file.write('[project]\nversion = "1.5.2.dev"\n')
+
+        result = sp.run([BUMP, self.tmp_file.name, "-D"])
+        version = toml.load(self.tmp_file.name)["project"]["version"]
+
+        assert version == "1.5.2"
