5de5d71d1841030e4f0da793c609687a7db3eba3
Lucas L. S. Haine <lucaslshaine@gmail.com>
2023-04-05 02:49:19 -0300

    fix: split version string

--- bumpline/separate_version.py
+++ bumpline/separate_version.py
@@ -27,8 +27,13 @@ def get_release(version: str):
 def get_pre_release(version: str):
     exp = PRE_RELEASE_EXPRESSION
     if not exp.match(version):
-        return version, ""
+        return "", version
     index = exp.match(version).end()
+
+    if version.find(".post") != -1:
+        index = version.find(".post")
+    elif version.find(".dev") != -1:
+        index = version.find(".dev")
 
     pre_release = version[:index]
     rest = version[index:]
@@ -39,8 +44,11 @@ def get_pre_release(version: str):
 def get_post_release(version: str):
     exp = POST_RELEASE_EXPRESSION
     if not exp.match(version):
-        return version, ""
+        return "", version
     index = exp.match(version).end()
+
+    if version.find(".dev") != -1:
+        index = version.find(".dev")
 
     post_release = version[:index]
     rest = version[index:]
@@ -51,22 +59,16 @@ def get_post_release(version: str):
 def get_dev_release(version: str):
     index = version.find(".dev")
     if index == -1:
-        return version, ""
+        return ""
 
-    rest = version[:index]
-    dev_release = version[index:]
-
-    return rest, dev_release
+    return version[index:]
 
 
 def split_version_string(version: str):
     epoch, version = get_epoch(version)
     release, version = get_release(version)
     pre_release, version = get_pre_release(version)
-    post_release, version  = get_post_release(version)
-    version, dev_release = get_dev_release(version)
+    post_release, dev_release = get_post_release(version)
+    #dev_release = get_dev_release(version)
 
     return epoch, release, pre_release, post_release, dev_release
-
-
-print(split_version_string("1!2.3.4rc5.post6.dev7"))
